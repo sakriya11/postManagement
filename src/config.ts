32c3f36cfd83e005cad098d290687a54d2a8c92c
Sakriya Khadka <khadkasakriya81@gmail.com>
2025-05-23 00:28:27 +0545

    production url changed

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const API_URL_LOCAL = "http://localhost:3000/api";
-export const API_URL = "https://post-management-backend-beige.vercel.app/";
+export const API_URL = "https://postmanagementbackend.onrender.com";
