78ccff073dd12f05bb5448a45340e7bb915a5f3d
Sakriya Khadka <khadkasakriya81@gmail.com>
2025-05-22 23:51:54 +0545

    new url added

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
-export const API_URL = "http://localhost:3000/api";
+export const API_URL_LOCAL = "http://localhost:3000/api";
+export const API_URL = "https://post-management-backend-beige.vercel.app/";
