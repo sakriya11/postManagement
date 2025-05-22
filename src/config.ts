fba70febd723b02e72d2e14364d5cc8931ad2d5f
Sakriya Khadka <khadkasakriya81@gmail.com>
2025-05-23 05:16:41 +0545

    changes in production url

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const API_URL_LOCAL = "http://localhost:3000/api";
-export const API_URL = "https://postmanagementbackend.onrender.com";
+export const API_URL = "https://postmanagementbackend.onrender.com/api";
